72f397594734aabc32c1e0014a5407c60cea1707
Milo Hartsoe <milo.red.duck@gmail.com>
2016-10-02 06:23:36 +0000

    Up and down non sync

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
-$(document).ready(function() {
+function start() {
 var textBeg = '<a-entity material="color: blue" ';
 var pos = 'position=';
 var rot = 'rotation=';
-var endOpen = 'mixin="font" text="text: Hello" ';
-var falling = 'animation__yoyo="property: position; dir: alternate; dur: 1000; easing: easeInSine; loop: true;' ;
-var fallingTo = ' to: ';
+var endOpen = 'mixin="font" text="text: 01011" ';
+var falling = 'animation__yoyo="property: position; dir: alternate; easing: easeInSine; loop: true;' ;
+var dur = 'dur: ';
+var fallingTo = '; to: ';
 var textEnd = '></a-animation><a-animation attribute="material.color" begin="click" to="red"></a-animation><a-box opacity="0" height="0.5" width="1.5" depth="0" position="0.75 0.25 0"></a-box></a-entity>';
 var startHeight = 3;
 var endHeight = 0;
@@ -21,6 +22,7 @@ for(var i=0; i<8;i++){
     item+= '"0 '+ coordinates[i][2] + ' 0" ';
     item+=endOpen;
     item+=falling;
+    item+=dur + (1000 + 3000*Math.random());
     item+= fallingTo + coordinates[i][0] + ' ' + endHeight + ' '+ coordinates[i][1] + '" ';
     item+= textEnd;
     console.log(item);
