2628b168e5f612ea1a9be77eb26836d90a29dc82
Milo Hartsoe <milo.red.duck@gmail.com>
2016-10-02 06:51:27 +0000

    stuff. I am tired.

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-function start() {
+$(document).ready(function() {
 var textBeg = '<a-entity material="color: blue" ';
 var pos = 'position=';
 var rot = 'rotation=';
-var endOpen = 'mixin="font" text="text: 01011" ';
+var endOpen = 'mixin="font" text="text: 01010101" ';
 var falling = 'animation__yoyo="property: position; dir: alternate; easing: easeInSine; loop: true;' ;
 var dur = 'dur: ';
 var fallingTo = '; to: ';
-var textEnd = '></a-animation><a-animation attribute="material.color" begin="click" to="red"></a-animation><a-box opacity="0" height="0.5" width="1.5" depth="0" position="0.75 0.25 0"></a-box></a-entity>';
+var textEnd = '></a-animation><a-animation attribute="material.color" begin="click" to="red"></a-animation><a-box opacity="0" height="0.5" width="2.5" depth="0" position="0.75 0.25 0"></a-box></a-entity>';
 var startHeight = 3;
 var endHeight = 0;
-var d = 3
+var d = 4;
 //coordinates are x,z,rotation
 var coordinates = [[-d/2,-d,0],[d/2,-d,0],[d,-d/2,-90],[d,d/2,-90],
                   [-d/2,d,180],[d/2,d,180],[-d,-d/2,90],[-d,d/2,90]];
