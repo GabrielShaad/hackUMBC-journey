3278fb3a3b2ff93db18b7a9981be5402d481c531
Milo Hartsoe <milo.red.duck@gmail.com>
2016-10-02 05:53:35 +0000

    falling

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
-$( document ).ready(function() {
-      
+function start() {
 var textBeg = '<a-entity material="color: blue" ';
 var pos = 'position=';
 var rot = 'rotation=';
-var textEnd = 'mixin="font" text="text: Hello"><a-animation attribute="material.color" begin="click" to="red"></a-animation><a-box opacity="0" height="0.5" width="1.5" depth="0" position="0.75 0.25 0"></a-box></a-entity>';
-var startHeight = 1.5;
+var endOpen = 'mixin="font" text="text: Hello">';
+var fallingFrom = '<a-animation attribute="position" from="'
+var fallingTo = ' to="'
+var textEnd = '></a-animation><a-animation attribute="material.color" begin="click" to="red"></a-animation><a-box opacity="0" height="0.5" width="1.5" depth="0" position="0.75 0.25 0"></a-box></a-entity>';
+var startHeight = 3;
+var endHeight = 0;
 var d = 3
 //coordinates are x,z,rotation
 var coordinates = [[-d/2,-d,0],[d/2,-d,0],[d,-d/2,-90],[d,d/2,-90],
@@ -16,10 +19,13 @@ for(var i=0; i<8;i++){
     item+= '"' + coordinates[i][0] + ' ' + startHeight +' ' + coordinates[i][1] + '" ';
     item+= rot;
     item+= '"0 '+ coordinates[i][2] + ' 0" ';
-    item += textEnd;
+    item+=endOpen;
+    item+=fallingFrom + coordinates[i][0] + ' ' + startHeight +' ' + coordinates[i][1] + '" ';
+    item+= fallingTo + coordinates[i][0] + ' ' + endHeight + ' '+ coordinates[i][1] + '" ';
+    item+= textEnd;
     console.log(item);
     
     $('#text').append(item);
 }
 console.log('done');
-});
+}
