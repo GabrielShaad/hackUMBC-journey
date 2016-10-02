a42d7a92b629b9b34d69978a58df61c6f02cf7a3
Milo Hartsoe <milo.red.duck@gmail.com>
2016-10-02 05:03:16 +0000

    Put script in main.js

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+$( document ).ready(function() {
+      
 var textBeg = '<a-entity material="color: blue" ';
 var pos = 'position=';
 var rot = 'rotation=';
@@ -6,9 +8,9 @@ var startHeight = 1.5;
 var d = 3
 //coordinates are x,z,rotation
 var coordinates = [[-d/2,-d,0],[d/2,-d,0],[d,-d/2,-90],[d,d/2,-90],
-                  [-d/2,d,180],[d/2,d,180],[-d,-d/2,-90],[-d,d/2,-90]];
+                  [-d/2,d,180],[d/2,d,180],[-d,-d/2,90],[-d,d/2,90]];
 //var textCont = $('.text');
-for(var i=0; i<6;i++){
+for(var i=0; i<8;i++){
     var item = textBeg;
     item+=pos;
     item+= '"' + coordinates[i][0] + ' ' + startHeight +' ' + coordinates[i][1] + '" ';
@@ -20,3 +22,4 @@ for(var i=0; i<6;i++){
     $('#text').append(item);
 }
 console.log('done');
+});
